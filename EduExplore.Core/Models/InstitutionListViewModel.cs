e0a53f73e8b61127b05e72f400c893ef4d2b1648
karinaghristova <karina.hristova@abv.bg>
2022-11-15 23:17:49 +0200

    Added view model

---
+++ EduExplore.Core/Models/InstitutionListViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduExplore.Core.Models
+{
+    public class InstitutionListViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string District { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string Municipality { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string InhabitedArea { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string Region { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string InstitutionType { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string DetailedInstitutionType { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string FinancialType { get; set; }
+    }
+}
+
