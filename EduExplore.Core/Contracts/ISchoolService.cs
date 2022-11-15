acea3bee1a9dbe58cec94660f9661926f3291292
karinaghristova <karina.hristova@abv.bg>
2022-11-16 00:14:15 +0200

    Added doc comments

--- EduExplore.Core/Contracts/ISchoolService.cs
+++ EduExplore.Core/Contracts/ISchoolService.cs
@@ -4,29 +4,82 @@ namespace EduExplore.Core.Contracts
 {
     public interface ISchoolService
     {
+        /// <summary>
+        /// Returns a list of all schools
+        /// </summary>
+        /// <returns></returns>
         Task<IEnumerable<InstitutionListViewModel>> GetAllSchools();
+        /// <summary>
+        /// Returns the number of all schools
+        /// </summary>
+        /// <returns></returns>
         Task<int> GetNumberOfAllSchools();
 
-        //By Inhabited area
+        /// <summary>
+        /// Returns a list of all schools in the given inhabited area
+        /// </summary>
+        /// <param name="inhabitedAreaId">Id of the inhabited area</param>
+        /// <returns></returns>
         Task<IEnumerable<InstitutionListViewModel>> GetAllSchoolsByInhabitedArea(string inhabitedAreaId);
+        /// <summary>
+        /// Returns the number of schools in a given inhabited area
+        /// </summary>
+        /// <param name="inhabitedAreaId">Id of the inhabited area</param>
+        /// <returns></returns>
         Task<int> GetNumberOfAllSchoolsInInhabitedArea(string inhabitedAreaId);
 
-        //By Region
+        /// <summary>
+        /// Returns a list of schools in the given region
+        /// </summary>
+        /// <param name="regionId">Id of the region</param>
+        /// <returns></returns>
         Task<IEnumerable<InstitutionListViewModel>> GetAllSchoolsByRegion(string regionId);
+        /// <summary>
+        /// Returns the number of all schools in the given region
+        /// </summary>
+        /// <param name="regionId">Id of the region</param>
+        /// <returns></returns>
         Task<int> GetNumberOfAllSchoolsInRegion(string regionId);
 
 
-        //By Institution type
+        /// <summary>
+        /// Returns a list of all schools from the given institution type
+        /// </summary>
+        /// <param name="institutionTypeId">Id of the institution type</param>
+        /// <returns></returns>
         Task<IEnumerable<InstitutionListViewModel>> GetAllSchoolsByInstitutionType(string institutionTypeId);
+        /// <summary>
+        /// Returns the number of all schools from the given institution type
+        /// </summary>
+        /// <param name="institutionTypeId">Id of the institution type</param>
+        /// <returns></returns>
         Task<int> GetNumberOfAllSchoolsOfInstitutionType(string institutionTypeId);
 
 
-        //By Detailed institution type
+        /// <summary>
+        /// Returns a list of all schools from the given detailed institution type
+        /// </summary>
+        /// <param name="detailedInstitutionTypeId">Id of the detailed institution type</param>
+        /// <returns></returns>
         Task<IEnumerable<InstitutionListViewModel>> GetAllSchoolsByDetailedInstitutionType(string detailedInstitutionTypeId);
+        /// <summary>
+        /// Returns the number of all schools from the given detailed institution type
+        /// </summary>
+        /// <param name="detailedInstitutionTypeId">Id of the detailed institution type</param>
+        /// <returns></returns>
         Task<int> GetNumberOfAllSchoolsOfDetailedInstitutionType(string detailedInstitutionTypeId);
 
-        //By Financial Type
+        /// <summary>
+        /// Returns a list of all schools from the given financial type
+        /// </summary>
+        /// <param name="financialTypeId">Id of the financial type</param>
+        /// <returns></returns>
         Task<IEnumerable<InstitutionListViewModel>> GetAllSchoolsByFinancialType(string financialTypeId);
+        /// <summary>
+        /// Returns the number of all schools from the given financial type
+        /// </summary>
+        /// <param name="financialTypeId">Id of the financial type</param>
+        /// <returns></returns>
         Task<int> GetNumberOfAllSchoolsOfFinancialType(string financialTypeId);
     }
 }
