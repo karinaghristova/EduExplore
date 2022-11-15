89de6deb37af52681eaa649ae2c16bddc9282178
karinaghristova <karina.hristova@abv.bg>
2022-11-15 03:36:25 +0200

    Input model added

---
+++ EduExplore.DataSeeder/InputModels/InstitutionInputModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduExplore.DataSeeder.InputModels
+{
+    public class InstitutionInputModel
+    {
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string District { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string Municipality { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string InhabitedArea { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string Region { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string InstitutionType { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string DetailedInstitutionType { get; set; }
+
+        [Required]
+        [StringLength(250)]
+
+        public string FinancialType { get; set; }
+    }
+}
+
