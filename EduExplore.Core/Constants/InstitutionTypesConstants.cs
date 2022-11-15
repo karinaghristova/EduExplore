355e392f1c630cf1222f077f13328894657957e7
karinaghristova <karina.hristova@abv.bg>
2022-11-15 23:18:07 +0200

    Added constants for institution types

---
+++ EduExplore.Core/Constants/InstitutionTypesConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduExplore.Core.Constants
+{
+    public class InstitutionTypesConstants
+    {
+        public const string Kindergarten = "детска градина";
+
+        public const string Nonspecialized = "неспециализирано";
+
+        public const string Specialized = "специализирано";
+
+        public const string Special = "специално";
+
+        public const string LackOfFreedom = "училище към местата за лишаване от свобода";
+    }
+}
