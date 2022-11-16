29896431a76deb6c7815e1f03ec7a949d4019eaa
karinaghristova <karina.hristova@abv.bg>
2022-11-17 00:40:18 +0200

    Added characteristic view model

---
+++ EduExplore/Models/AllNamedCharacteristicsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using EduExplore.Core.Models;
+
+namespace EduExplore.Models
+{
+    public class AllNamedCharacteristicsViewModel
+    {
+        public IEnumerable<NamedCharacteristicListViewModel> Characteristics { get; set; }
+        public int CountOfCharacteristics => Characteristics.Count();
+    }
+}
