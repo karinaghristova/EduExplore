b2cb5ffdb75b7c00d53793c793b46d40788ca47a
karinaghristova <karina.hristova@abv.bg>
2022-11-16 04:46:34 +0200

    Added School controller

---
+++ EduExplore/Controllers/SchoolController.cs
@@ -0,0 +1,41 @@
+﻿using EduExplore.Core.Constants;
+using EduExplore.Core.Contracts;
+using EduExplore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduExplore.Controllers
+{
+    public class SchoolController : Controller
+    {
+        private readonly ISchoolService schoolService;
+
+        public SchoolController(ISchoolService schoolService)
+        {
+            this.schoolService = schoolService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AllSchools(int page = 1)
+        {
+            var schools = await schoolService.GetAllSchools();
+            int schoolsPerPage = PageConstants.PageSize20;
+            int schoolsToSkip = page == 1 ? 0 : ((page - 1) * schoolsPerPage);
+
+
+            return View(new AllSchoolsViewModel
+            {
+                Schools = schools.Skip(schoolsToSkip).Take(schoolsPerPage),
+                Paging = new PagingViewModel
+                {
+                    CurrentPage = page,
+                    TotalItems = schools.Count(),
+                    PageSize = schoolsPerPage
+                }
+            });
+        }
+    }
+}
