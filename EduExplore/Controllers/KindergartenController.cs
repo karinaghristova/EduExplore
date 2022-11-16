440cf436fb363e468a5588c6d8bc3c6e43c6c036
karinaghristova <karina.hristova@abv.bg>
2022-11-16 04:46:23 +0200

    Added Kindergarten controller

---
+++ EduExplore/Controllers/KindergartenController.cs
@@ -0,0 +1,41 @@
+﻿using EduExplore.Core.Constants;
+using EduExplore.Core.Contracts;
+using EduExplore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduExplore.Controllers
+{
+    public class KindergartenController : Controller
+    {
+        private readonly IKindergartenService kindergartenService;
+
+        public KindergartenController(IKindergartenService kindergartenService)
+        {
+            this.kindergartenService = kindergartenService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AllKindergartens(int page = 1)
+        {
+            var kindergartens = await kindergartenService.GetAllKindergartens();
+            int kindergartensPerPage = PageConstants.PageSize20;
+            int kindergartensToSkip = page == 1 ? 0 : ((page - 1) * kindergartensPerPage);
+
+
+            return View(new AllKindergartensViewModel
+            {
+                Kindergartens = kindergartens.Skip(kindergartensToSkip).Take(kindergartensPerPage),
+                Paging = new PagingViewModel
+                {
+                    CurrentPage = page,
+                    TotalItems = kindergartens.Count(),
+                    PageSize = kindergartensPerPage
+                }
+            });
+        }
+    }
+}
