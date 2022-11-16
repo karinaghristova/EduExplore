c5442a06c3e2795848599b40a8f318b9cb13a7a0
karinaghristova <karina.hristova@abv.bg>
2022-11-17 00:40:36 +0200

    Added view model for schools

---
+++ EduExplore/Models/School/AllSchoolsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EduExplore.Core.Models;
+
+namespace EduExplore.Models
+{
+    public class AllSchoolsViewModel
+    {
+        public IEnumerable<InstitutionListViewModel> Schools { get; set; }
+
+        public PagingViewModel Paging { get; set; }
+
+        public string CriteriaId { get; set; }
+    }
+}
