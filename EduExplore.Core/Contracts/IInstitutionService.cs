1915564c4b992cf8cda2555c06b345c45f549d19
karinaghristova <karina.hristova@abv.bg>
2022-11-17 17:33:06 +0200

    Added some methods to the institution service

--- EduExplore.Core/Contracts/IInstitutionService.cs
+++ EduExplore.Core/Contracts/IInstitutionService.cs
@@ -4,24 +4,167 @@ namespace EduExplore.Core.Contracts
 {
     public interface IInstitutionService
     {
+        /// <summary>
+        /// Returns a list of all inhabited areas
+        /// </summary>
+        /// <returns></returns>
         Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllInhabitedAreas();
 
+        /// <summary>
+        /// Returns the number of all inhabited areas
+        /// </summary>
+        /// <returns></returns>
         Task<int> GetNumberOfAllInhabitedAreas();
+
+        /// <summary>
+        /// Returns a list of all regions
+        /// </summary>
+        /// <returns></returns>
 
         Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllRegions();
 
+       
+        /// <summary>
+        /// Returns the number of all regions
+        /// </summary>
+        /// <returns></returns>
         Task<int> GetNumberOfAllRegions();
 
+        /// <summary>
+        /// Returns a list of all institution types
+        /// </summary>
+        /// <returns></returns>
         Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllInstitutionTypes();
 
+       
+        /// <summary>
+        /// Returns the number of all institution types
+        /// </summary>
+        /// <returns></returns>
         Task<int> GetNumberOfAllInstitutionTypes();
 
+        /// <summary>
+        /// Returns a list of all detailed institution types
+        /// </summary>
+        /// <returns></returns>
         Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllDetailedInstitutionTypes();
 
+        /// <summary>
+        /// Returns the number of all detailed institution types
+        /// </summary>
+        /// <returns></returns>
         Task<int> GetNumberOfAllDetailedInstitutionTypes();
 
+       
+        /// <summary>
+        /// Returns a list of all financial types 
+        /// </summary>
+        /// <returns></returns>
         Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllFinancialTypes();
 
+
+        /// <summary>
+        /// Returns the number of all financial types
+        /// </summary>
+        /// <returns></returns>
         Task<int> GetNumberOfAllFinancialTypes();
+
+        //For schools
+        /// <summary>
+        /// Returns a list of all inhabited areas where schools are present
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllInhabitedAreasForSchools();
+
+        /// <summary>
+        /// Returns the number of all inhabited areas where schools are present
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllInhabitedAreasForSchools();
+
+        /// <summary>
+        /// Returns a list of all regions where schools are present
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllRegionsForSchools();
+
+        /// <summary>
+        /// Returns the number of all regions where schools are present
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllRegionsForSchools();
+
+        /// <summary>
+        /// Returns a list of all institution types related to schools
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllInstitutionTypesForSchools();
+
+        /// <summary>
+        /// Returns the number of all institution types related to school
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllInstitutionTypesForSchools();
+
+        /// <summary>
+        /// Returns a list of all detailed institution types related to schools
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllDetailedInstitutionTypesForSchools();
+
+        /// <summary>
+        /// Returns the number of all detailed institution types related to schools
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllDetailedInstitutionTypesForSchools();
+
+        /// <summary>
+        /// Returns a list of all financial types that schools have
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllFinancialTypesForSchools();
+
+        /// <summary>
+        /// Returns the number of all financial types that schools have
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllFinancialTypesForSchools();
+
+        //For kindergartens
+        /// <summary>
+        /// Returns a list of all inhabited areas where kindergartens are present
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllInhabitedAreasForKindergartens();
+
+        /// <summary>
+        /// Returns the number of all inhabited areas where kindergartens are present
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllInhabitedAreasForKindergartens();
+
+        /// <summary>
+        /// Returns a list of all regions where kindergartens are present
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllRegionsForKindergartens();
+
+        /// <summary>
+        /// Returns the number of all regions where kinergartens are present
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllRegionsForKindergartens();
+
+        /// <summary>
+        /// Returns a list of all financial types that kindergartens have
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllFinancialTypesForKindergartens();
+
+        /// <summary>
+        /// Returns the number of all financial types that kindergartens have 
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllFinancialTypesForKindergartens();
     }
 }
--- EduExplore.Core/Services/InstitutionService.cs
+++ EduExplore.Core/Services/InstitutionService.cs
@@ -1,4 +1,5 @@
-﻿using EduExplore.Core.Contracts;
+﻿using EduExplore.Core.Constants;
+using EduExplore.Core.Contracts;
 using EduExplore.Core.Models;
 using EduExplore.Infrastructure.Data.DbModels;
 using EduExplore.Infrastructure.Data.Repositories;
@@ -31,6 +32,18 @@ namespace EduExplore.Core.Services
                 .ToListAsync();
         }
 
+        public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllDetailedInstitutionTypesForSchools()
+        {
+            return await repo.All<DetailedInstitutionType>()
+                .Where(dit => dit.Name != InstitutionTypesConstants.Kindergarten)
+                .Select(dit => new NamedCharacteristicListViewModel
+                {
+                    Id = dit.Id.ToString(),
+                    Name = dit.Name
+                })
+                .ToListAsync();
+        }
+
         public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllFinancialTypes()
         {
             return await repo.All<FinancialType>()
@@ -42,9 +55,61 @@ namespace EduExplore.Core.Services
                 .ToListAsync();
         }
 
+        public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllFinancialTypesForKindergartens()
+        {
+            return await repo.All<FinancialType>()
+               .Include(i => i.Institutions)
+               .Where(x => x.Institutions.Any(i => i.InstitutionType.Name == InstitutionTypesConstants.Kindergarten))
+               .Select(ft => new NamedCharacteristicListViewModel
+               {
+                   Id = ft.Id.ToString(),
+                   Name = ft.Name
+               })
+               .ToListAsync();
+        }
+
+        public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllFinancialTypesForSchools()
+        {
+            return await repo.All<FinancialType>()
+               .Include(i => i.Institutions)
+               .Where(x => x.Institutions.Any(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten))
+               .Select(ft => new NamedCharacteristicListViewModel
+               {
+                   Id = ft.Id.ToString(),
+                   Name = ft.Name
+               })
+               .ToListAsync();
+        }
+
         public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllInhabitedAreas()
         {
             return await repo.All<InhabitedArea>()
+                .Select(i => new NamedCharacteristicListViewModel
+                {
+                    Id = i.Id.ToString(),
+                    Name = i.Name
+                })
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllInhabitedAreasForKindergartens()
+        {
+            return await repo.All<InhabitedArea>()
+                .Include(i => i.Institutions)
+                .Where(x => x.Institutions.Any(i => i.InstitutionType.Name == InstitutionTypesConstants.Kindergarten))
+                .Select(i => new NamedCharacteristicListViewModel
+                {
+                    Id = i.Id.ToString(),
+                    Name = i.Name
+                })
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllInhabitedAreasForSchools()
+        {
+            return await repo.All<InhabitedArea>()
+                .Include(i => i.Institutions)
+                .Where(x => x.Institutions.Any(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten))
                 .Select(i => new NamedCharacteristicListViewModel
                 {
                     Id = i.Id.ToString(),
@@ -64,6 +129,18 @@ namespace EduExplore.Core.Services
                 .ToListAsync();
         }
 
+        public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllInstitutionTypesForSchools()
+        {
+            return await repo.All<InstitutionType>()
+                .Where(x => x.Name != InstitutionTypesConstants.Kindergarten)
+                .Select(it => new NamedCharacteristicListViewModel
+                {
+                    Id = it.Id.ToString(),
+                    Name = it.Name
+                })
+                .ToListAsync();
+        }
+
         public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllRegions()
         {
             return await repo.All<Region>()
@@ -75,9 +152,41 @@ namespace EduExplore.Core.Services
                 .ToListAsync();
         }
 
+        public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllRegionsForKindergartens()
+        {
+            return await repo.All<Region>()
+               .Include(i => i.Institutions)
+               .Where(x => x.Institutions.Any(i => i.InstitutionType.Name == InstitutionTypesConstants.Kindergarten))
+               .Select(r => new NamedCharacteristicListViewModel
+               {
+                   Id = r.Id.ToString(),
+                   Name = r.Name
+               })
+               .ToListAsync();
+        }
+
+        public async Task<IEnumerable<NamedCharacteristicListViewModel>> GetAllRegionsForSchools()
+        {
+            return await repo.All<Region>()
+               .Include(i => i.Institutions)
+               .Where(x => x.Institutions.Any(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten))
+               .Select(r => new NamedCharacteristicListViewModel
+               {
+                   Id = r.Id.ToString(),
+                   Name = r.Name
+               })
+               .ToListAsync();
+        }
+
         public async Task<int> GetNumberOfAllDetailedInstitutionTypes()
         {
             var detailedInstitutionTypes = await GetAllDetailedInstitutionTypes();
+            return detailedInstitutionTypes.Count();
+        }
+
+        public async Task<int> GetNumberOfAllDetailedInstitutionTypesForSchools()
+        {
+            var detailedInstitutionTypes = await GetAllDetailedInstitutionTypesForSchools();
             return detailedInstitutionTypes.Count();
         }
 
@@ -87,9 +196,33 @@ namespace EduExplore.Core.Services
             return financialTypes.Count();
         }
 
+        public async Task<int> GetNumberOfAllFinancialTypesForKindergartens()
+        {
+            var financialTypes = await GetAllFinancialTypesForKindergartens();
+            return financialTypes.Count();
+        }
+
+        public async Task<int> GetNumberOfAllFinancialTypesForSchools()
+        {
+            var financialTypes = await GetAllFinancialTypesForSchools();
+            return financialTypes.Count();
+        }
+
         public async Task<int> GetNumberOfAllInhabitedAreas()
         {
             var inhabitedAreas = await GetAllInhabitedAreas();
+            return inhabitedAreas.Count();
+        }
+
+        public async Task<int> GetNumberOfAllInhabitedAreasForKindergartens()
+        {
+            var inhabitedAreas = await GetAllInhabitedAreasForKindergartens();
+            return inhabitedAreas.Count();
+        }
+
+        public async Task<int> GetNumberOfAllInhabitedAreasForSchools()
+        {
+            var inhabitedAreas = await GetAllInhabitedAreasForSchools();
             return inhabitedAreas.Count();
         }
 
@@ -99,9 +232,27 @@ namespace EduExplore.Core.Services
             return institutionTypes.Count();
         }
 
+        public async Task<int> GetNumberOfAllInstitutionTypesForSchools()
+        {
+            var institutionTypes = await GetAllInstitutionTypesForSchools();
+            return institutionTypes.Count();
+        }
+
         public async Task<int> GetNumberOfAllRegions()
         {
             var regions = await GetAllRegions();
+            return regions.Count();
+        }
+
+        public async Task<int> GetNumberOfAllRegionsForKindergartens()
+        {
+            var regions = await GetAllRegionsForKindergartens();
+            return regions.Count();
+        }
+
+        public async Task<int> GetNumberOfAllRegionsForSchools()
+        {
+            var regions = await GetAllRegionsForSchools();
             return regions.Count();
         }
     }
