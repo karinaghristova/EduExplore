3e2b6e5c4b65bd21b88a535f4269243f5c3f0ae1
karinaghristova <karina.hristova@abv.bg>
2022-11-17 19:02:59 +0200

    Institution controller school methods added

--- EduExplore/Controllers/InstitutionController.cs
+++ EduExplore/Controllers/InstitutionController.cs
@@ -20,7 +20,7 @@ namespace EduExplore.Controllers
 
         public async Task<IActionResult> AllInhabitedAreasSchool()
         {
-            var inhabitedAreas = await institutionService.GetAllInhabitedAreas();
+            var inhabitedAreas = await institutionService.GetAllInhabitedAreasForSchools();
 
             return View(new AllNamedCharacteristicsViewModel
             {
@@ -30,7 +30,7 @@ namespace EduExplore.Controllers
 
         public async Task<IActionResult> AllRegionsSchool()
         {
-            var inhabitedAreas = await institutionService.GetAllRegions();
+            var inhabitedAreas = await institutionService.GetAllRegionsForSchools();
 
             return View(new AllNamedCharacteristicsViewModel
             {
@@ -38,9 +38,42 @@ namespace EduExplore.Controllers
             });
         }
 
+        public async Task<IActionResult> AllInstitutionTypesSchool()
+        {
+            var institutionTypes = await institutionService.GetAllInstitutionTypesForSchools();
+
+            return View(new AllNamedCharacteristicsViewModel
+            {
+                Characteristics = institutionTypes
+            });
+        }
+
+        public async Task<IActionResult> AllDetailedInstitutionTypesSchool()
+        {
+            var detailedInstitutionTypes = await institutionService.GetAllDetailedInstitutionTypesForSchools();
+
+            return View(new AllNamedCharacteristicsViewModel
+            {
+                Characteristics = detailedInstitutionTypes
+            });
+        }
+
+        public async Task<IActionResult> AllFinancialTypesSchool()
+        {
+            var financialTypes = await institutionService.GetAllFinancialTypesForSchools();
+
+            return View(new AllNamedCharacteristicsViewModel
+            {
+                Characteristics = financialTypes
+            });
+        }
+
+
+        //For kindergartens
+
         public async Task<IActionResult> AllInhabitedAreasKindergarten()
         {
-            var inhabitedAreas = await institutionService.GetAllInhabitedAreas();
+            var inhabitedAreas = await institutionService.GetAllInhabitedAreasForKindergartens();
 
             return View(new AllNamedCharacteristicsViewModel
             {
