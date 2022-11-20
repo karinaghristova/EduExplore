30679d61bc0cfb83defd31de9fda4790344619f5
karinaghristova <karina.hristova@abv.bg>
2022-11-20 21:24:47 +0200

    Created statistics service

---
+++ EduExplore.Core/Contracts/IStatisticsService.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduExplore.Core.Contracts
+{
+    public interface IStatisticsService
+    {
+        /// <summary>
+        /// Returns the number of all inhabited areas
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllInhabitedAreas();
+
+        /// <summary>
+        /// Returns the number of all regions
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllRegions();
+
+        /// <summary>
+        /// Returns the number of all institution types
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllInstitutionTypes();
+
+        /// <summary>
+        /// Returns the number of all detailed institution types
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllDetailedInstitutionTypes();
+
+        /// <summary>
+        /// Returns the number of all financial types
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllFinancialTypes();
+
+        //Schools
+        /// <summary>
+        /// Returns the number of all inhabited areas where schools are present
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllInhabitedAreasForSchools();
+
+        /// <summary>
+        /// Returns the number of all regions where schools are present
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllRegionsForSchools();
+
+        /// <summary>
+        /// Returns the number of all institution types related to school
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllInstitutionTypesForSchools();
+
+        /// <summary>
+        /// Returns the number of all detailed institution types related to schools
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllDetailedInstitutionTypesForSchools();
+
+        /// <summary>
+        /// Returns the number of all financial types that schools have
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllFinancialTypesForSchools();
+
+        /// <summary>
+        /// Returns the number of all schools
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllSchools();
+
+        /// <summary>
+        /// Returns the number of schools in a given inhabited area
+        /// </summary>
+        /// <param name="inhabitedAreaId">Id of the inhabited area</param>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllSchoolsInInhabitedArea(string inhabitedAreaId);
+
+        /// <summary>
+        /// Returns the number of all schools in the given region
+        /// </summary>
+        /// <param name="regionId">Id of the region</param>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllSchoolsInRegion(string regionId);
+
+        /// <summary>
+        /// Returns the number of all schools from the given institution type
+        /// </summary>
+        /// <param name="institutionTypeId">Id of the institution type</param>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllSchoolsOfInstitutionType(string institutionTypeId);
+
+        /// <summary>
+        /// Returns the number of all schools from the given detailed institution type
+        /// </summary>
+        /// <param name="detailedInstitutionTypeId">Id of the detailed institution type</param>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllSchoolsOfDetailedInstitutionType(string detailedInstitutionTypeId);
+
+        /// <summary>
+        /// Returns the number of all schools from the given financial type
+        /// </summary>
+        /// <param name="financialTypeId">Id of the financial type</param>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllSchoolsOfFinancialType(string financialTypeId);
+
+
+
+        //Kindergartens
+        /// <summary>
+        /// Returns the number of all inhabited areas where kindergartens are present
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllInhabitedAreasForKindergartens();
+
+        /// <summary>
+        /// Returns the number of all regions where kinergartens are present
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllRegionsForKindergartens();
+
+        /// <summary>
+        /// Returns the number of all financial types that kindergartens have 
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllFinancialTypesForKindergartens();
+
+        /// <summary>
+        /// Returns the number of all kindergartens
+        /// </summary>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllKindergartens();
+
+        /// <summary>
+        /// Returns the number of kindergartens in the inhabited area
+        /// </summary>
+        /// <param name="inhabitedAreaId">Id of the inhabited area</param>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllKindergartensInInhabitedArea(string inhabitedAreaId);
+
+        /// <summary>
+        /// Returns the number of all kindergartens in the given region
+        /// </summary>
+        /// <param name="regionId">Id of the region</param>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllKindergartensInRegion(string regionId);
+
+        /// <summary>
+        /// Returns the number of all kindergartens from the givven financial type
+        /// </summary>
+        /// <param name="financialTypeId">id of the financial type</param>
+        /// <returns></returns>
+        Task<int> GetNumberOfAllKindergartensOfFinancialType(string financialTypeId);
+    }
+}
---
+++ EduExplore.Core/Services/StatisticsService.cs
@@ -0,0 +1,211 @@
+﻿using EduExplore.Core.Constants;
+using EduExplore.Core.Contracts;
+using EduExplore.Infrastructure.Data.DbModels;
+using EduExplore.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduExplore.Core.Services
+{
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly IApplicationDbRepository repo;
+
+        public StatisticsService(IApplicationDbRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        //Educational institutions overall
+        public async Task<int> GetNumberOfAllInhabitedAreas()
+        {
+            var inhabitedAreas = await repo.All<InhabitedArea>().ToListAsync();
+            return inhabitedAreas.Count();
+        }
+
+        public async Task<int> GetNumberOfAllRegions()
+        {
+            var regions = await repo.All<Region>().ToListAsync();
+            return regions.Count();
+        }
+
+        public async Task<int> GetNumberOfAllInstitutionTypes()
+        {
+            var institutionTypes = await repo.All<InstitutionType>().ToListAsync();
+            return institutionTypes.Count();
+        }
+
+        public async Task<int> GetNumberOfAllDetailedInstitutionTypes()
+        {
+            var dit = await repo.All<DetailedInstitutionType>().ToListAsync();
+            return dit.Count();
+        }
+
+        public async Task<int> GetNumberOfAllFinancialTypes()
+        {
+            var financialTypes = await repo.All<FinancialType>().ToListAsync();
+            return financialTypes.Count();
+        }
+
+        //Schools
+        public async Task<int> GetNumberOfAllInhabitedAreasForSchools()
+        {
+            var inhabitedAreas = await repo.All<InhabitedArea>()
+                .Include(i => i.Institutions)
+                .Where(x => x.Institutions.Any(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten))
+                .ToListAsync();
+            return inhabitedAreas.Count();
+        }
+
+        public async Task<int> GetNumberOfAllRegionsForSchools()
+        {
+            var regions = await repo.All<Region>()
+                .Include(i => i.Institutions)
+                .Where(x => x.Institutions.Any(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten))
+                .ToListAsync();
+            return regions.Count();
+        }
+
+        public async Task<int> GetNumberOfAllInstitutionTypesForSchools()
+        {
+            var institutionTypes = await repo.All<InstitutionType>()
+                .Include(i => i.Institutions)
+                .Where(x => x.Institutions.Any(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten))
+                .ToListAsync();
+            return institutionTypes.Count();
+        }
+
+        public async Task<int> GetNumberOfAllDetailedInstitutionTypesForSchools()
+        {
+            var ditSchools = await repo.All<DetailedInstitutionType>()
+                .Include(i => i.Institutions)
+                .Where(x => x.Institutions.Any(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten))
+                .ToListAsync();
+            return ditSchools.Count();
+        }
+
+        public async Task<int> GetNumberOfAllFinancialTypesForSchools()
+        {
+            var financialTypes = await repo.All<FinancialType>()
+                .Include(i => i.Institutions)
+                .Where(x => x.Institutions.Any(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten))
+                .ToListAsync();
+            return financialTypes.Count();
+        }
+
+        public async Task<int> GetNumberOfAllSchools()
+        {
+            var schools = await repo.All<Institution>()
+                .Where(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten)
+                .ToListAsync();
+            return schools.Count();
+        }
+
+        public async Task<int> GetNumberOfAllSchoolsInInhabitedArea(string inhabitedAreaId)
+        {
+            var schools = await repo.All<Institution>()
+              .Where(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten &&
+               i.InhabitedAreaId == new Guid(inhabitedAreaId))
+              .ToListAsync();
+            return schools.Count();
+        }
+
+        public async Task<int> GetNumberOfAllSchoolsInRegion(string regionId)
+        {
+            var schools = await repo.All<Institution>()
+              .Where(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten &&
+               i.RegionId == new Guid(regionId))
+              .ToListAsync();
+            return schools.Count();
+        }
+
+        public async Task<int> GetNumberOfAllSchoolsOfInstitutionType(string institutionTypeId)
+        {
+            var schools = await repo.All<Institution>()
+              .Where(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten &&
+               i.InstitutionTypeId == new Guid(institutionTypeId))
+              .ToListAsync();
+            return schools.Count();
+        }
+
+        public async Task<int> GetNumberOfAllSchoolsOfDetailedInstitutionType(string detailedInstitutionTypeId)
+        {
+            var schools = await repo.All<Institution>()
+              .Where(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten &&
+               i.DetailedInstitutionTypeId == new Guid(detailedInstitutionTypeId))
+              .ToListAsync();
+            return schools.Count();
+        }
+
+        public async Task<int> GetNumberOfAllSchoolsOfFinancialType(string financialTypeId)
+        {
+            var schools = await repo.All<Institution>()
+              .Where(i => i.InstitutionType.Name != InstitutionTypesConstants.Kindergarten &&
+               i.FinancialTypeId == new Guid(financialTypeId))
+              .ToListAsync();
+            return schools.Count();
+        }
+
+        //Kindergartens
+        public async Task<int> GetNumberOfAllInhabitedAreasForKindergartens()
+        {
+            var inhabitedAreas = await repo.All<InhabitedArea>()
+                .Include(i => i.Institutions)
+                .Where(x => x.Institutions.Any(i => i.InstitutionType.Name == InstitutionTypesConstants.Kindergarten))
+                .ToListAsync();
+            return inhabitedAreas.Count();
+        }
+
+        public async Task<int> GetNumberOfAllRegionsForKindergartens()
+        {
+            var regions = await repo.All<Region>()
+                .Include(i => i.Institutions)
+                .Where(x => x.Institutions.Any(i => i.InstitutionType.Name == InstitutionTypesConstants.Kindergarten))
+                .ToListAsync();
+            return regions.Count();
+        }
+
+        public async Task<int> GetNumberOfAllFinancialTypesForKindergartens()
+        {
+            var financialTypes = await repo.All<FinancialType>()
+                .Include(i => i.Institutions)
+                .Where(x => x.Institutions.Any(i => i.InstitutionType.Name == InstitutionTypesConstants.Kindergarten))
+                .ToListAsync();
+            return financialTypes.Count();
+        }
+
+        public async Task<int> GetNumberOfAllKindergartens()
+        {
+            var kindergartens = await repo.All<Institution>()
+               .Where(i => i.InstitutionType.Name == InstitutionTypesConstants.Kindergarten)
+               .ToListAsync();
+            return kindergartens.Count();
+        }
+
+        public async Task<int> GetNumberOfAllKindergartensInInhabitedArea(string inhabitedAreaId)
+        {
+            var kindergartens = await repo.All<Institution>()
+              .Where(i => i.InstitutionType.Name == InstitutionTypesConstants.Kindergarten &&
+               i.InhabitedAreaId == new Guid(inhabitedAreaId))
+              .ToListAsync();
+            return kindergartens.Count();
+        }
+
+        public async Task<int> GetNumberOfAllKindergartensInRegion(string regionId)
+        {
+            var kindergartens = await repo.All<Institution>()
+              .Where(i => i.InstitutionType.Name == InstitutionTypesConstants.Kindergarten &&
+               i.RegionId == new Guid(regionId))
+              .ToListAsync();
+            return kindergartens.Count();
+        }
+
+        public async Task<int> GetNumberOfAllKindergartensOfFinancialType(string financialTypeId)
+        {
+            var kindergartens = await repo.All<Institution>()
+               .Where(i => i.InstitutionType.Name == InstitutionTypesConstants.Kindergarten &&
+                i.FinancialTypeId == new Guid(financialTypeId))
+               .ToListAsync();
+            return kindergartens.Count();
+        }
+    }
+}
