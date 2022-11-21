8fad1c89d4f11805d5a520d37bcabc35a4d6b963
karinaghristova <karina.hristova@abv.bg>
2022-11-21 05:07:54 +0200

    Added view for statistics

---
+++ EduExplore/Models/Statistics/SchoolsVsKindergartensViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EduExplore.Core.Constants;
+
+namespace EduExplore.Models.Statistics
+{
+    public class SchoolsVsKindergartensStatisticsViewModel
+    {
+        public int AllSchoolsCount { get; set; }
+        public int AllKindergartensCount { get; set; }
+        public int NumberOfInhabitedAreasWithSchools { get; set; }
+        public int NumberOfInhabitedAreasWithKindergartens { get; set; }
+    }
+}
