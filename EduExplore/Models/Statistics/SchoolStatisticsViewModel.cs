3754030d776efb3ce161534399fa2cad838cb343
karinaghristova <karina.hristova@abv.bg>
2022-11-21 05:07:16 +0200

    Added school statistics

---
+++ EduExplore/Models/Statistics/SchoolStatisticsViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace EduExplore.Models.Statistics
+{
+    public class SchoolStatisticsViewModel
+    {
+        public int AllSchoolsCount { get; set; }
+        public int SchoolsCountInSofiaCity { get; set; }
+        public List<string> InhabitedAreasNames { get; set; }
+        public List<int> SchoolsCountInInhabitedAreasList { get; set; }
+        public List<string> RegionsNames { get; set; }
+        public List<int> SchoolsCountInRegionsList { get; set; }
+        public List<string> InstitutionTypesNames { get; set; }
+        public List<int> SchoolsCountOfInstitutionTypesList { get; set; }
+        public List<string> DetailedInstitutionTypesNames { get; set; }
+        public List<int> SchoolsCountOfDetailedInstitutionTypesList { get; set; }
+        public List<string> FinancialTypesNames { get; set; }
+        public List<int> SchoolsCountOfFinancialTypesList { get; set; }
+    }
+}
