c2c6f1dffb4f93415234d7f159471a7b66eed403
karinaghristova <karina.hristova@abv.bg>
2022-11-21 05:06:53 +0200

    Added kindergarten statistics

---
+++ EduExplore/Models/Statistics/KindergartenStatisticsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EduExplore.Models.Statistics
+{
+    public class KindergartenStatisticsViewModel
+    {
+        public int AllKindergartensCount { get; set; }
+        public int KindergartensCountInSofiaCity { get; set; }
+        public List<string> InhabitedAreasNames { get; set; }
+        public List<int> KindergartensCountInInhabitedAreasList { get; set; }
+        public List<string> RegionsNames { get; set; }
+        public List<int> KindergartensCountInRegionsList { get; set; }
+        public List<string> FinancialTypesNames { get; set; }
+        public List<int> KindergartensCountOfFinancialTypesList { get; set; }
+    }
+}
